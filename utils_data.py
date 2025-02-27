7af72aa48884277a88d05380677e800405affebe
Mihály Orsós <ormraat.pte@gmail.com>
2025-02-27 07:32:53 +0100

    Update utils_data.py

--- utils_data.py
+++ utils_data.py
@@ -11,15 +11,15 @@ def translate_day(day):
     return day_translation.get(day, day)
 
 # Caching the Tw object to optimize performance
-@st.cache_data(ttl=600)
+@st.cache_data(ttl=60*60*2)
 def get_data():
-    #df = pd.read_csv('https://raw.githubusercontent.com/misrori/flights/main/csv_data/last_prices.csv')
-    df = pd.read_csv('last_prices.csv')
+    df = pd.read_csv('https://raw.githubusercontent.com/misrori/flights/main/csv_data/last_prices.csv')
+    #df = pd.read_csv('last_prices.csv')
 
     
     # Load data (replace with actual data sources)
-    #airports = pd.read_csv('https://raw.githubusercontent.com/misrori/flights/main/csv_data/airportrs.csv')
-    airports = pd.read_csv('airportrs.csv')
+    airports = pd.read_csv('https://raw.githubusercontent.com/misrori/flights/main/csv_data/airportrs.csv')
+    #airports = pd.read_csv('airportrs.csv')
 
     #df['link'] = df['link'].apply(create_link)
     df['indulas_nap'] = df['indulas_nap'].apply(translate_day)
